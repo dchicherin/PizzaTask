cd9a8719ae91c200f93223ecf98b97918c4353d7
Dmitrii <navssane@gmail.com>
2024-01-12 22:35:58 +0300

    Initial Commit

---
+++ PizzaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaApp
+//
+//  Created by Dmitry Chicherin on 12/1/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
